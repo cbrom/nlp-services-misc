9eb6ed1b967e6c980b221916cc01855dfa06db65
cbrom_a <kb.cbrom@gmail.com>
2019-04-10 09:16:10 +0300

    changed port numbers so that it doesn't conflict with others

--- semantic-role-labeling/run_docker.sh
+++ semantic-role-labeling/run_docker.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 sudo docker build --file Dockerfile . -t singularitynet:srl_allen
-sudo docker run -d -v /home/zelalem/nlp-services-misc/allen-srl/etcd:/semantic-role-labeling-service/etcd -v /etc/letsencrypt:/etc/letsencrypt -it -p 8018:8018 -p 8008:8008 --name semantic-role-labeling-service singularitynet:semantic-role-labeling-service
+sudo docker run -d -v /home/zelalem/nlp-services-misc/allen-srl/etcd:/semantic-role-labeling-service/etcd -v /etc/letsencrypt:/etc/letsencrypt -it -p 8021:8021 -p 8011:8011 --name semantic-role-labeling-service singularitynet:semantic-role-labeling-service
