5797c9cfa3af2e73703325046aeae6cab6b6f4a5
cbrom_a <kb.cbrom@gmail.com>
2019-04-10 10:01:26 +0300

    exception handling and error messages updated

--- semantic-role-labeling/service/server.py
+++ semantic-role-labeling/service/server.py
@@ -26,31 +26,23 @@ class SRLServicer(srl_pb2_grpc.SRLServicer):
 
         result = allen_srl.SRL.get_srl(request.document)
 
-        for verbs_dic in result['verbs']:
-            # ver = srl_pb2.Verbs(verb=verbs_dic['verb'], verbs_dic['description'])
-            ver = srl_pb2.Verbs()
-            for tag in verbs_dic['tags']:
-                ver.tags.word.append(tag)
-            response.verbs.add(verb=verbs_dic['verb'], description=verbs_dic['description'], tags=ver.tags)
-        for word in result['words']:
-            response.words.word.append(word)
-        
-        
+        if result is None:
+            context.set_code(grpc.StatusCode.FAILED_PRECONDITION)
+            context.set_details("document is invalid.")
+            return srl_pb2.Output()
 
-
-        # verbs
-        # verbs_list = []
-        # for verb_dic in result['verbs']:
-        #     verbMessage = srl_pb2.Verb(
-        #         verb=verb_dic['verb'], 
-        #         description=verb_dic['description'],
-        #         tags=verb_dic['tags'])
-        #     verbs_list.append(verbMessage)
-
-        # response = srl_pb2.Output(
-        #     verbs=verbs_list,
-        #     words=result['words']
-        # )
+        try:
+            for verbs_dic in result['verbs']:
+                ver = srl_pb2.Verbs()
+                for tag in verbs_dic['tags']:
+                    ver.tags.word.append(tag)
+                response.verbs.add(verb=verbs_dic['verb'], description=verbs_dic['description'], tags=ver.tags)
+            for word in result['words']:
+                response.words.word.append(word)
+        except Exception as exp:
+            context.set_code(grpc.StatusCode.UNKNOWN)
+            context.set_details("Exception happened during getting results... \n", exp.__str__())
+                return srl_pb2.Output()
 
         return response
 
